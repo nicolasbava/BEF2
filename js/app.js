ea02af7eeb2313fcba5d12c41560c8290b700f2f
nicolasbava <nicobava96@gmail.com>
2022-03-30 20:21:03 -0300

    agregue slider cv

---
+++ js/app.js
@@ -0,0 +1,42 @@
+var check = document.querySelector(".check");
+
+check.addEventListener('click', idioma);
+
+function idioma(){
+   let id = check.checked;
+   if(id === false){
+       location.href="en/index.html"
+   } else {
+        location.href="../index.html"
+   }
+}
+
+
+/* SLIDER CV WISNY */
+
+(function() {
+    const sliders = [...document.querySelectorAll('.slider_body')];
+    const arrowNext = document.querySelector('#after');
+    const arrowBefore = document.querySelector('#before');
+
+    let value;
+
+    arrowNext.addEventListener('click', ()=>changePosition(1));
+    arrowBefore.addEventListener('click', ()=>changePosition(-1));
+
+    function changePosition(change){
+        const currentElement = Number(document.querySelector('.slider_body-show').dataset.id)
+
+        value = currentElement
+        value += change
+        
+        if(value===0 || value == sliders.length+1) {
+            value = value === 0 ? sliders.length : 1
+        }
+
+        sliders[currentElement-1].classList.toggle('slider_body-show')
+
+        sliders[value-1].classList.toggle('slider_body-show')
+    }
+
+})()
