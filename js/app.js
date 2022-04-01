9a62db4edf4f9f36ad4df801dc7b024bebe640f9
nicolasbava <nicobava96@gmail.com>
2022-04-01 20:33:13 -0300

    primer entregable

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ check.addEventListener('click', idioma);
 function idioma(){
    let id = check.checked;
    if(id === false){
-       location.href="en/index.html"
+       location.href="./es/index.html"
    } else {
         location.href="../index.html"
    }
@@ -40,3 +40,22 @@ function idioma(){
     }
 
 })()
+
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
