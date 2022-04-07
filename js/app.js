94371c09ae75913bfcf89cee29467da6a4795292
nicolasbava <nicobava96@gmail.com>
2022-04-06 22:05:22 -0300

    segundo entregable

--- js/app.js
+++ js/app.js
@@ -11,36 +11,6 @@ function idioma(){
    }
 }
 
-
-/* SLIDER CV WISNY */
-
-(function() {
-    const sliders = [...document.querySelectorAll('.slider_body')];
-    const arrowNext = document.querySelector('#after');
-    const arrowBefore = document.querySelector('#before');
-
-    let value;
-
-    arrowNext.addEventListener('click', ()=>changePosition(1));
-    arrowBefore.addEventListener('click', ()=>changePosition(-1));
-
-    function changePosition(change){
-        const currentElement = Number(document.querySelector('.slider_body-show').dataset.id)
-
-        value = currentElement
-        value += change
-        
-        if(value===0 || value == sliders.length+1) {
-            value = value === 0 ? sliders.length : 1
-        }
-
-        sliders[currentElement-1].classList.toggle('slider_body-show')
-
-        sliders[value-1].classList.toggle('slider_body-show')
-    }
-
-})()
-
 const buttons = document.querySelectorAll("[data-carousel-button]")
 
 buttons.forEach(button => {
@@ -59,3 +29,7 @@ buttons.forEach(button => {
     delete activeSlide.dataset.active
   })
 })
+ 
+
+
+  
